9087683cb002f046ee4fcd3c1f4590c8ebdfc30b
abagusw <wisnueka11@gmail.com>
2021-09-05 19:33:31 +0700

    fix typo user_types_id to user_type_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
     protected $fillable = [
         'email',
         'password',
-        'user_types_id'
+        'user_type_id'
     ];
 
     protected $hidden = [
